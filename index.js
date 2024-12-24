b15fe2b479e8f415c0b117d2c233fc9299915172
karndiy <karndiy@gmail.com>
2024-12-24 10:29:08 +0700

    up

--- index.js
+++ index.js
@@ -2,12 +2,29 @@ const { Client } = require('pg');
 
 // Set up the connection configuration
 const client = new Client({
-  user:  'my_database_psk5_user',// 'postgres',
-  host:  'dpg-ctl1e3i3esus73ecgro0-a',// 'localhost',
-  database: 'my_database_psk5',//'my_database',
+  user:  'node_pg_w9mi_user',// 'postgres',
+  host:  'dpg-ctl2imbv2p9s738csqv0-a',// 'localhost',
+  database: 'node_pg_w9mi',//'my_database',
   password: 'em8L3NqvObSvfaPmTDJF5QaYfZmUsy0X',//'your_password',
   port: 5432,
 });
+
+
+// Hostname
+// An internal hostname used by your Render services.
+
+// dpg-ctl2imbv2p9s738csqv0-a
+
+// Port
+// 5432
+
+// Database
+// node_pg_w9mi
+
+// Username
+// node_pg_w9mi_user
+
+
 
 // Connect to PostgreSQL
 client.connect()
